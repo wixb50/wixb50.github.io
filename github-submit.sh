0f30ac535bf834d67ebc9fb8e029dc870da75f52
谢文倩 <wixb50@163.com>
2015-11-07 22:54:46 +0800

    submit message

---
+++ github-submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#1.first you need generate the static website.
+./hugo server -b "http://wixb50.github.io/" --appendPort=false &
+sleep 2
+pkill hugo
+
+#2.then submit the source file to develop branch.
+git add content
+git commit -m "submit message"
+git push origin develop
+
+#3.then submit the static web to master branch.
+cd public
+git add -A
+git commit -m "submit message"
+git push origin master
+
+#finish
