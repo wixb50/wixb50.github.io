c8cbccc47c334a75001540c67b12f9af385dcc0c
谢文倩 <wixb50@163.com>
2015-11-17 20:19:13 +0800

    test

--- github-submit.sh
+++ github-submit.sh
@@ -5,22 +5,23 @@
 sleep 2
 pkill hugo
 #input the submit message
-while [ ! $msg ]
-do
-	echo "please input the submit message:"
-	read msg
-done
+echo "please input the submit message:"
+read msg
+if [ ! $msg ];then
+	msg="submit message"
+fi
+echo $msg
 
 #2.then submit the source file to develop branch.
 git add -A
 git reset HEAD public
-git commit -m $msg
+git commit -m "$msg"
 git push origin develop
 
 #3.then submit the static web to master branch.
 cd public
 git add -A
-git commit -m $msg
+git commit -m "$msg"
 git push origin master
 
 #finish
