d80efe5ef75fa775104db351f3dd99181ea8ea05
谢文倩 <wixb50@163.com>
2016-03-24 15:31:29 +0800

    submit message

--- coding-submit.sh
+++ coding-submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #1.first you need generate the static website.
-./hugo server -b "http://wixb50.gitcafe.io/" --appendPort=false &
+./hugo server -b "http://wixb50.coding.me/" --appendPort=false &
 sleep 2
 pkill hugo
 #input the submit message
@@ -17,7 +17,7 @@ cd public
 git add -A
 git commit -m "$msg"
 # 添加 gitcafe 远程源,强制推送到gitcafe
-git remote add gitcafe https://gitcafe.com/wixb50/wixb50.git
-git push -f gitcafe master:gitcafe-pages
+git remote add coding https://git.coding.net/wixb50/wixb50.git
+git push -f coding master:coding-pages
 
 #finish
